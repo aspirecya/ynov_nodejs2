210c4476d859ce6b581d7077fdf934a98eff8ec9
wassim <wassim.matougui@ynov.com>
2019-01-18 13:06:45 +0100

    création models client & referent

---
+++ models/client.model.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+let clientSchema =  new Schema ({
+    nom : Number,
+    adresse : String,
+    cp : String,
+    ville : Number,
+    referentId : Number,
+    tel : Number,
+    mail : String,
+    prospect : Boolean
+});
+
+// export schema to mongodb database
+module.exports = mongoose.model('Client', clientSchema);
---
+++ models/referent.model.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+let referentSchema =  new Schema ({
+    id : Number,
+    nom : String,
+    prenom : String,
+    poste : String
+});
+
+// export schema to mongodb database
+module.exports = mongoose.model('Referent', referentSchema);
